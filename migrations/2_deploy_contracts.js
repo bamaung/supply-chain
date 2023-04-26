5667a3c4ca7387a36d2b9f77a01c1d85ba3ed839
Ye Myat Oo <yemyatoo@lambdr.com>
2023-04-27 02:39:05 +0800

    update 2_deploy_contracts.js script

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,14 +1,22 @@
 // migrating the appropriate contracts
+var Roles = artifacts.require("./Roles.sol");
 var FarmerRole = artifacts.require("./FarmerRole.sol");
 var DistributorRole = artifacts.require("./DistributorRole.sol");
 var RetailerRole = artifacts.require("./RetailerRole.sol");
 var ConsumerRole = artifacts.require("./ConsumerRole.sol");
 var SupplyChain = artifacts.require("./SupplyChain.sol");
+var Ownable = artifacts.require("./Ownable.sol");
 
 module.exports = function(deployer) {
+  deployer.deploy(Roles);
+  deployer.link(Roles, FarmerRole);
+  deployer.link(Roles, DistributorRole);
+  deployer.link(Roles, RetailerRole);
+  deployer.link(Roles, ConsumerRole);
   deployer.deploy(FarmerRole);
   deployer.deploy(DistributorRole);
   deployer.deploy(RetailerRole);
   deployer.deploy(ConsumerRole);
+  deployer.deploy(Ownable);
   deployer.deploy(SupplyChain);
 };
