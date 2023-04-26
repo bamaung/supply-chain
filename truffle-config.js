84b525dcd49ac39ce7b4c8944af16e2b70aad8cc
Ye Myat Oo <yemyatoo@lambdr.com>
2023-04-26 23:31:53 +0800

    update truffle conifg for testnet

--- truffle-config.js
+++ truffle-config.js
@@ -1,9 +1,20 @@
+const HDWalletProvider = require("truffle-hdwallet-provider");
+const fs = require('fs');
+const homedir = require('os').homedir();
+const mnemonic = fs.readFileSync(homedir + "/.dev_seeds/testnet").toString().trim();
+
 module.exports = {
   networks: {
     development: {
       host: "127.0.0.1",
       port: 8545,
       network_id: "*" // Match any network id
+    },
+    sepolia: {
+      provider: function() { 
+        return new HDWalletProvider(mnemonic, "https://sepolia.infura.io/v3/2d907fb6564d494585bbcd668f253c73");
+      },
+      network_id: "11155111",
     }
   }
 };
